665f1094af0f085e525caf082235d5ef1aad9ae4
Egan Sutherland <egans@nevada.unr.edu>
2019-12-09 22:04:30 -0800

    builds images from an ordered directory of images. not reliant on opencv currently, will be fixed later

--- ImageBuilder.py
+++ ImageBuilder.py
@@ -1,15 +1,46 @@
-import cv2 as cv
 import numpy as np
-#import matplotlib.pyplot as plt
 import os
 import sys
+from PIL import Image
 
-directory = sys.argv[1]
-images = []
+#combines images in an ordered set into a single image. Goes through
+#input directory, placing images from left to right, top to bottom into
+#a new image
+#
+#In current form it does not use opencv for image manipulation due to
+#installation problems. Will be fixed
+def BuildImage(X, Y, inputDirectory, outputDirectory='Output/', outputName='image.png'):
+	images = []
 
-for index in os.listdir(directory):
-	images.append(cv.imread(directory+index))
+	for index in os.listdir(inputDirectory):
+		#temp = np.array(plt.imread(inputDirectory + index)).astype(np.float)
+		images.append(Image.open(inputDirectory + index))
 
-dims = images[0].shape
-print(dims)
-cv.imshow('img',images[1])
+	numIms = len(images)
+	width, height = images[0].size
+	
+	if (X * Y) > numIms:
+		print('error')
+		return
+
+	total_width = width * X
+	total_height = height * Y
+
+	outputImage = Image.new('RGB',(total_width,total_height))
+
+	x_offset = 0
+	y_offset = 0
+	count = 0
+
+	for i in range(0, Y):
+		for j in range(0,X):
+			outputImage.paste(images[count],(x_offset,y_offset))
+			x_offset += width
+			count += 1
+		x_offset = 0
+		y_offset += height
+		
+	if not os.path.exists(outputDirectory):
+		os.makedirs(outputDirectory)
+
+	outputImage.save(outputDirectory + outputName)
