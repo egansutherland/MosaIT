0ec593f542fffcca84a1bf5d447de39bc2ccc580
Egan Sutherland <egans@nevada.unr.edu>
2020-01-21 17:33:11 -0800

    added command for chromedriver, need to edit for you system

--- driver.py
+++ driver.py
@@ -1,12 +1,14 @@
 import image_scraper
 import image_builder
 import sys
+import os
 import math
+# from PIL import Image
 
-#keyword limit widthpx heightpx X Y
-#dft =	100 100 100 5 5
+# keyword limit widthpx heightpx X Y
+# dft =	100 100 100 5 5
 
-#options are >400*300, >640*480, >800*600, >1024*768
+# options are >400*300, >640*480, >800*600, >1024*768
 searchSize = ">400*300"
 
 if len(sys.argv) < 2:
@@ -29,5 +31,14 @@ if len(sys.argv) > 6:
 	X = int(sys.argv[5])
 	Y = int(sys.argv[6])
 
+justInCase = 0
+
 image_scraper.search(keyword,limit, width, height, searchSize)
+while (len(os.listdir("Output/"+keyword+"/")) <= 0) and (justInCase < 100):
+	print("false start, attempt number: " + str(justInCase))
+	image_scraper.search(keyword,limit, width, height, searchSize)
+	justInCase += 1
+	
 image_builder.BuildImage(X,Y, "Output/"+keyword+"/", outputDirectory="Mosaic/" + keyword + "/")
+# im = Image.open("Mosaic/" + keyword + "/image.png")
+# im.show()
