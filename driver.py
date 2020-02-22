283fd928a64cc3d73b2563c389f25399e3b99f76
Jared Manning <Jared@Jareds-MacBook-Pro-7.local>
2020-02-21 17:47:23 -0800

    Cleaned up how it takes command line arguments, added more options, changed how/where it saves files, allows multiple runs without cleaning output

--- driver.py
+++ driver.py
@@ -4,68 +4,84 @@ import image_analyzer
 import image_ordering
 import TargetImage
 import Image
+
+import argparse
 import sys
 import os
 import math
 import cv2 as cv
 # from PIL import Image
 
-# keyword limit widthpx heightpx X Y
-# dft =	100 100 100 5 5
+parser = argparse.ArgumentParser(description='Calls MosaIT backend driver with parameters.')
 
-# options are >400*300, >640*480, >800*600, >1024*768
-searchSize = ">400*300"
+parser.add_argument("keyword", help = "the search term that gathered images correspond to")
+parser.add_argument("limit", help = "the number of images to attempt to gather", type = int)
+parser.add_argument("x", help = "number of images per row", type = int)
+parser.add_argument("y", help = "number of images per column", type = int)
+parser.add_argument("-s","--skip", help = "skips the image gathering", action = "store_true")
+parser.add_argument("-b","--best", help = "chooses best image, instead of first above threshold", action = "store_true")
+parser.add_argument("-r","--repeat", help = "allows repeat of images in mosaic", action = "store_true")
+parser.add_argument("-d","--delete", help = "clears everything in Downloads, Cropped, Grid, Ordered, and Mosaic directories", action = "store_true")
+parser.add_argument("-c","--colorSim", help = "sets a color similarity threshold", type = float, default = 0)
+parser.add_argument("-i","--input", help = "filename of desired target image in Input directory", default = os.listdir("Input/")[0])
 
-if len(sys.argv) < 2:
-	print("Error: no keyword entered")
-	exit()
-keyword = sys.argv[1]
+#get all the args
+args = parser.parse_args()
+keyword = args.keyword
 keyword = keyword.replace("_"," ")
-limit = 100
-if len(sys.argv) > 2:
-	limit = sys.argv[2]
-width = 100
-height = 100
-if len(sys.argv) > 4:
-	width = int(sys.argv[3])
-	height = int(sys.argv[4])
+limit = args.limit
+x = args.x
+y = args.y
+skip = args.skip
+best = args.best #TODO
+repeat = args.repeat #TODO
+colorSim = args.colorSim
+targetImageFile = args.input
+targetImageFileName = targetImageFile.split(".")[0]
 
-x = 5
-y = 5
-if len(sys.argv) > 6:
-	x = int(sys.argv[5])
-	y = int(sys.argv[6])
+if args.delete:
+	os.system("sh cleanOutput.sh")
 
-colorSim = 0
-if len(sys.argv) > 7:
-	colorSim = float(sys.argv[7])
-
-
-targetImagePath = os.listdir("Input/")[0]
-#image_analyzer.gridify(X,Y,targetImagePath)
-
-targetImage = TargetImage.TargetImage("Input/"+targetImagePath, x, y)
+#open targetImage and find height and width
+targetImage = TargetImage.TargetImage("Input/"+targetImageFile, x, y)
 height = targetImage.grid[0].image.shape[0]
 width = targetImage.grid[0].image.shape[1]
-downloadTries = 0
+print(width,height)
+#obtain images and put in Downloads/keyword directory
+if not skip:
+	downloadTries = 0
+	searchSize = ">400*300" #probably remove this
+	image_scraper.search(keyword, limit, searchSize)
+	while (len(os.listdir("Downloads/"+keyword+"/")) <= 0) and (downloadTries < 100):
+		print("false start, attempt number: " + str(downloadTries))
+		image_scraper.search(keyword, limit, searchSize)
+		downloadTries += 1
 
-image_scraper.search(keyword,limit, width, height, searchSize)
-while (len(os.listdir("Output/"+keyword+"/")) <= 0) and (downloadTries < 100):
-	print("false start, attempt number: " + str(downloadTries))
-	image_scraper.search(keyword,limit, width, height, searchSize)
-	downloadTries += 1
+#crop downloaded images and put into Cropped/keyword directory
+image_scraper.crop(keyword, width, height)
 
+#turn all downloaded images into Image class
 downloadImages = []
-for download in os.listdir("Output/"+keyword+"/"):
-	downloadImage = Image.Image("Output/"+keyword+"/"+download, None)
+for download in os.listdir("Cropped/"+keyword+"/"):
+	downloadImage = Image.Image("Cropped/"+keyword+"/"+download, None)
 	downloadImages.append(downloadImage)
 
+#order images accordingly
 orderedImages = image_ordering.OrderImages(targetImage,downloadImages, colorSim)
 
+#save ordered images to directory (for debugging)
+orderedDir = "Ordered/" + keyword + "/"
+try:
+	os.mkdir(orderedDir)
+except:
+	print(orderedDir,"exists... removing files")
+	for f in os.listdir(orderedDir):
+		os.remove(orderedDir + f)
 for n,ordImg in enumerate(orderedImages):
-	cv.imwrite("Ordered/"+keyword+"/"+str(n)+".png",ordImg.image)
+	cv.imwrite(orderedDir+str(n)+".png",ordImg.image)
 
-image_builder.BuildImage(x,y, orderedImages, colorSim, outputDirectory="Mosaic/" + keyword + "/")
+#build mosaic out of ordered images
+image_builder.BuildImage(x,y, orderedImages, colorSim, outputDirectory="Mosaic/" + keyword + "/", outputName=targetImageFileName)
 
 # im = Image.open("Mosaic/" + keyword + "/image.png")
 # im.show()
