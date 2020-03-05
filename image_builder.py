b33324cd24acc9f679fed73c9e5c2f421722235a
Jared Manning <Jared@Jareds-MacBook-Pro-7.local>
2020-03-05 14:06:57 -0800

    takes less arguments

--- image_builder.py
+++ image_builder.py
@@ -15,37 +15,15 @@ import cv2 as cv
 # inputDirectory: directory containing input images
 # outputDirectory: directory to output images to
 # outputName: desired name of output image
-def BuildImage(x, y, inputImages, colorSim, outputDirectory='Output/', outputName='image'):
+def BuildImage(x, y, inputImages, outputDirectory='Output/', outputName='image'):
 	images = []
 
-	# for index in os.listdir(inputDirectory):
-	# 	#temp = np.array(plt.imread(inputDirectory + index)).astype(np.float)
-	# 	images.append(Image.open(inputDirectory + index))
-
 	numIms = len(inputImages)
-	# height = inputImages[0].image.shape[0]
-	# width = inputImages[0].image.shape[1]
 	
 	if (x * y) > numIms:
 		print('Error: not enough images, needed ' + str(x*y) + " but got " + str(numIms))
 		return
 
-	#total_width = width * X
-	#total_height = height * Y
-
-	#outputImage = Image.new('RGB',(total_width,total_height))
-
-	# x_offset = 0
-	# y_offset = 0
-	# count = 0
-
-	# for i in range(0, Y):
-	# 	for j in range(0,X):
-	# 		outputImage.paste(inputImages[count].image,(x_offset,y_offset))
-	# 		x_offset += width
-	# 		count += 1
-	# 	x_offset = 0
-	# 	y_offset += height
 	outputImage = None
 	counter = 0
 	col = []
@@ -68,6 +46,6 @@ def BuildImage(x, y, inputImages, colorSim, outputDirectory='Output/', outputNam
 	if not os.path.exists(outputDirectory):
 		os.makedirs(outputDirectory)
 
-	mosaicName = outputDirectory + outputName + str(x) + "_" + str(y) + "_" + str(colorSim) + ".png"
-	cv.imwrite(outputDirectory + outputName + str(x) + "_" + str(y) + "_" + str(colorSim) + ".png", outputImage)
+	mosaicName = outputDirectory + outputName
+	cv.imwrite(mosaicName, outputImage)
 	print("Success,",mosaicName,"created!")
