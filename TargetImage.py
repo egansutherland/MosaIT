f64c0cd00778ce879ffc5f39f31a0412d0e29396
Egan Sutherland <egans@nevada.unr.edu>
2020-03-10 18:26:54 -0700

    logging hopefully

--- TargetImage.py
+++ TargetImage.py
@@ -1,10 +1,11 @@
 import cv2 as cv
 import Image
 import image_analyzer
+import sys
 
 class TargetImage:
 	def __init__(self, filepath, x, y):
-		console.log(self.filepath)
+		sys.stdout.write(self.filepath)
 		self.filepath = filepath
 		self.image = cv.imread(filepath, cv.IMREAD_COLOR)
 		self.x = x
