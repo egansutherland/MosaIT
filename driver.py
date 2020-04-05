037344e464cbc253e4150a0bb16b9af1bfc0e197
Jared Manning <Jared@Jareds-MBP-8.attlocal.net>
2020-04-04 17:56:26 -0700

    added threads option, timings, and cleaned up and removed a lot of old code

--- driver.py
+++ driver.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import image_scraper
 import image_builder
-import image_analyzer
 import image_ordering
 import TargetImage
 import Image
@@ -12,25 +11,34 @@ import os
 import math
 import tempfile
 import cv2 as cv
+import time
 
 from PIL import Image as IMAGE
 
 parser = argparse.ArgumentParser(description='Calls MosaIT backend driver with parameters.')
 
+#required arguments
 parser.add_argument("keyword", help = "the search term that gathered images correspond to")
 parser.add_argument("limit", help = "the number of images to attempt to gather", type = int)
 parser.add_argument("x", help = "number of images per row", type = int)
 parser.add_argument("y", help = "number of images per column", type = int)
+
+#toggle options
 parser.add_argument("-s","--skip", help = "skips the image gathering", action = "store_true")
 parser.add_argument("-b","--best", help = "chooses best image, instead of first above threshold", action = "store_true")
 parser.add_argument("-r","--repeat", help = "allows repeat of images in mosaic", action = "store_true")
 parser.add_argument("-v","--view", help = "views the completed mosaic", action = "store_true")
-parser.add_argument("-d","--delete", help = "clears everything in Downloads, Cropped, Grid, Ordered, and Mosaic directories", action = "store_true")
+
+#options with values
 parser.add_argument("-c","--colorSim", help = "sets a color similarity threshold", type = float, default = 0)
 parser.add_argument("-i","--input", help = "filepath to target image", default = None)
-parser.add_argument("-o","--outputPath",help = "path to output Mosaic", default = "Mosaic/")
-parser.add_argument("-n","--outputName",help = "name of output file", default = None)
-parser.add_argument("-a", "--downloadsDirectory", help = "path to downloads if -s specified", default = "/root/MosaIT/Downloads/")
+parser.add_argument("-o","--outputPath", help = "path to output Mosaic", default = "Mosaic/")
+parser.add_argument("-n","--outputName", help = "name of output file", default = None)
+parser.add_argument("-a", "--databaseDirectory", help = "path to directory of images if -s is used", default = "/root/MosaIT/Downloads/") #"/root/MosaIT"
+parser.add_argument("-t", "--threads", help = "number of threads to use for source searching and image downloading", type=int, default=1)
+
+#start timer
+startTotalTime = time.perf_counter()
 
 #get all the args
 args = parser.parse_args()
@@ -40,104 +48,73 @@ limit = args.limit
 x = args.x
 y = args.y
 
+#get toggle options
 skip = args.skip
 best = args.best
 repeat = args.repeat
 view = args.view
 
+#get options with values
 colorSim = args.colorSim
 targetImageFile = args.input
 targetImageFileName = targetImageFile.split("/")[-1].split(".")[0]
-output = args.outputPath #output file path
+outputPath = args.outputPath #output file path
 outputName = args.outputName #output file name
-downloadsDirectory = args.downloadsDirectory
-
-if args.delete:
-	os.system("sh cleanOutput.sh")
+databaseDirectory = args.databaseDirectory #the directory of images to use if using skip
+threads = args.threads
 
 #open targetImage and find height and width
 if not "/" in targetImageFile:
 	targetImage = TargetImage.TargetImage("/root/MosaIT/Input/"+targetImageFile, x, y)
 else:
-	sys.stdout.write("target file path: " + targetImageFile + "\n")
+	sys.stdout.write("TargetImage path: " + targetImageFile + "\n")
 	targetImage = TargetImage.TargetImage(targetImageFile, x, y)
+totalHeight = targetImage.image.shape[0]
+totalWidth = targetImage.image.shape[1]
+print("Dimensions of target image (w,h): ", totalWidth, totalHeight)
 height = targetImage.grid[0].image.shape[0]
 width = targetImage.grid[0].image.shape[1]
-print("Dimensions of grid image: ", width, height)
+print("Dimensions of grid image (w,h): ", width, height)
 
-#make temp directory for downloads
-if (downloadsDirectory == None) and skip:
-	downloadsDirectory = tempfile.mkdtemp()
-	
 #obtain images and put in temp Downloads directory
-# if not skip:
-# 	downloadTries = 0
-# 	searchSize = ">400*300" #probably remove this
-# 	downloadImages = []								 #THIS IS A LIST OF IMAGE CLASS OBJECTS THAT SHOULD BE POPULATED FROM BELOW (NOT USED NOW)
-# 	image_scraper.search(keyword, downloadsDirectory, limit, searchSize) #IDEALLY THIS SHOULD RETURN A LIST OF IMAGE OBJECTS
-# 	while (len(os.listdir(downloadsDirectory)) <= 0) and (downloadTries < 100):	#IDEALLY IT CHECKS LEN OF LIST OF IMAGE OBJECTS (DEPENDENT)
-# 		print("false start, attempt number: " + str(downloadTries))
-# 		image_scraper.search(keyword, downloadsDirectory, limit, searchSize)								#THIS SHOULD BE ADJUSTED TOO
-# 		downloadTries += 1
-
-sources = []
 croppedImages =[]
-
 if not skip:
-	sources = image_scraper.search(keyword, limit)
-	croppedImages = image_scraper.download(downloadsDirectory, sources, width, height)
+	downloadsDirectory = tempfile.mkdtemp()
+	startSearchTime = time.perf_counter()
+	sources = image_scraper.search(keyword, limit, threads)
+	endSearchTime = time.perf_counter()
+	print("Search time: " + str(endSearchTime - startSearchTime) + "\n")
+	startDownloadTime = time.perf_counter()
+	croppedImages = image_scraper.download(downloadsDirectory, sources, width, height, limit, threads)
+	endDownloadTime = time.perf_counter()
+	print("Download time: " + str(endDownloadTime - startDownloadTime) + "\n")
 else:
-	croppedImages = image_scraper.crop(keyword, width, height, downloadsDirectory)
-
-#make temp directory for crop
-# cropDirectory = tempfile.mkdtemp()
-# files = os.listdir(cropDirectory)
-
-#crop downloaded images and put into Cropped/keyword directory
-# image_scraper.crop(keyword, width, height, downloadsDirectory, cropDirectory)
-# files = os.listdir(cropDirectory)
-
-#turn all downloaded images into Image class #INSTEAD OF CONVERTING TO IMAGE OBJECTS IT SHOULD CALL CROP ON EACH OBJECT (WIP)
-# croppedImages = []
-# for download in os.listdir(cropDirectory):						#IN DOWNLOADIMAGES
-# 	downloadImage = Image.Image(cropDirectory+"/"+download, None)	#CALL CROP METHOD
-# 	croppedImages.append(downloadImage)
+	croppedImages = image_scraper.cropDirectory(keyword, width, height, databaseDirectory)
 
 #order images accordingly
+startMosaicTime = time.perf_counter()
 orderedImages = image_ordering.OrderImages(targetImage,croppedImages, colorSim, best, repeat)
 
-#save ordered images to directory (for debugging)						#(NOT DEPENDENT)
-# orderedDir = "Ordered/" + keyword + "/"
-# try:
-# 	os.mkdir(orderedDir)
-# except:
-# 	print(orderedDir,"exists... removing files")
-# 	for f in os.listdir(orderedDir):
-# 		os.remove(orderedDir + f)
-# for n,ordImg in enumerate(orderedImages):
-# 	cv.imwrite(orderedDir+str(n)+".png",ordImg.image)
-
-#make the filename for the completed mosaic
-mosaicFileName = outputName
-
-if mosaicFileName == None:
-	mosaicFileName = targetImageFileName + str(x) + "_" + str(y)
+#make the filename for the completed mosaic (if not given)
+if outputName == None:
+	outputName = targetImageFileName + str(x) + "_" + str(y)
 	if best:
-		mosaicFileName += "_b"
+		outputName += "_b"
 	else:
-		mosaicFileName += "_" + str(colorSim)
+		outputName += "_" + str(colorSim)
 	if repeat:
-		mosaicFileName += "_r"
-	mosaicFileName += ".png"
+		outputName += "_r"
+	outputName += ".png"
 
 #build mosaic out of ordered images
-outputFilePath = output
-# if not (output == "Mosaic/"):
-# 	outputFilePath = output
-# else:
-# 	outputFilePath = output
-image_builder.BuildImage(x, y, orderedImages, outputDirectory=outputFilePath, outputName=mosaicFileName)
+image_builder.BuildImage(x, y, orderedImages, outputDirectory=outputPath, outputName=outputName)
+endMosaicTime = time.perf_counter()
+print("Mosaic Build Time: " + str(endMosaicTime - startMosaicTime)+"\n")
+
+#print total timer
+endTotalTime = time.perf_counter()
+print("Total Time: " + str(endTotalTime - startTotalTime))
 
 if view:
-	im = IMAGE.open(outputFilePath+"/"+mosaicFileName)	#DEPENDENT... kinda
+	im = IMAGE.open(outputPath+"/"+outputName)
 	im.show()
