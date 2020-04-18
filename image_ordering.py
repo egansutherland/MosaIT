0d1101ba464204914ec4bfbc99d1f9da98ea392d
Jared Manning <Jared@Jareds-MBP-8.attlocal.net>
2020-04-18 15:15:45 -0700

    added a threadedOrderImages, which is called only if threads>1, best is true, and repeat is true

--- image_ordering.py
+++ image_ordering.py
@@ -2,13 +2,18 @@ import numpy as np
 import cv2 as cv
 import Image
 import TargetImage
+import pymp
 
 # analyses target image and input images. Orders
 # input images by changing names based on where
 # it matches target image best.
 # target is of type TargetImage
 # inputImages is an array of type Image
-def OrderImages(target, inputImages, colorSimIn, best=False, repeat=False):
+def OrderImages(target, inputImages, colorSimIn, best=False, repeat=False, threads=1):
+	if best and repeat and threads > 1:
+		return threadedOrderImages(target, inputImages, threads)
+	else:
+		print("Using non-threaded ordering")
 	outputImages = []
 	for gridIm in target.grid:
 		if len(outputImages)%100 == 0 and len(outputImages) != 0:
@@ -39,3 +44,21 @@ def OrderImages(target, inputImages, colorSimIn, best=False, repeat=False):
 					except:
 						pass
 	return outputImages
+
+def threadedOrderImages(target, inputImages, threads):
+	print("Using threaded ordering")
+	numGridImages = len(target.grid)
+	outputImages = pymp.shared.list([None]*numGridImages)
+	with pymp.Parallel(threads) as p:
+		for gridIdx in p.range(0, numGridImages):
+			gridIm = target.grid[gridIdx]
+			imBest = None
+			colorSimBest = 0
+			for downloadIm in inputImages:
+				colorSim = gridIm.colorSimilarity(downloadIm)
+				if colorSim > colorSimBest:
+					colorSimBest = colorSim
+					imBest = downloadIm
+			outputImages[gridIdx]=imBest
+	return outputImages
+
