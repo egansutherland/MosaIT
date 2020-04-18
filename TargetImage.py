be71c34de62ee4b9385a7266b14aaf2ebed5a204
Jared Manning <Jared@Jareds-MBP-8.attlocal.net>
2020-04-18 14:02:58 -0700

    removed old code, and temp directory for grid images

--- TargetImage.py
+++ TargetImage.py
@@ -7,7 +7,6 @@ import os
 class TargetImage:
 	def __init__(self, filepath, x, y):
 		self.filepath = filepath
-		#sys.stdout.write(self.filepath)
 		self.image = cv.imread(filepath, cv.IMREAD_COLOR)
 		if self.image is None:
 			print ("Couldn't open TargetImage at " + filepath)
@@ -18,18 +17,13 @@ class TargetImage:
 	def gridify(self):
 		if self.image is None:
 			return None
-		#filename = filepath.split("/")[1]
-		#sys.stdout.write("filename: " + filename)
-		gridDir = tempfile.mkdtemp()
 
 		#slightly crop target image so divisible by x and y
-		#sys.stdout.write(filepath)
 		height = self.image.shape[0]
 		y_rem = height%self.y
 		width = self.image.shape[1]
 		x_rem = width%self.x
 		self.image=self.image[0:height-y_rem,0:width-x_rem]
-		#cv.imwrite(filepath,crop_im)
 		y_ticks = int(self.image.shape[0]/self.y)
 		x_ticks = int(self.image.shape[1]/self.x)
 
@@ -38,9 +32,7 @@ class TargetImage:
 		for i in range(self.y):
 			for j in range(self.x):
 				grid_im = self.image[i*y_ticks:(i+1)*y_ticks,j*x_ticks:(j+1)*x_ticks]
-				outpath = gridDir + '/' + str(i) + "_" + str(j) + ".png"
-				cv.imwrite(outpath,grid_im)
-				grids.append(Image.Image(outpath, None))
+				grids.append(Image.Image(None, None, image=grid_im))
 		return grids
 
 		
